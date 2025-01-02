4146adcc9dbfb1b3253b5c3af3b9fc0e50842a87
Manohar Singh <manoharsingh@Manohars-MacBook-Pro.local>
2025-01-02 19:05:39 +0530

    debounce autosuggestion

---
+++ debounce/index.js
@@ -0,0 +1,30 @@
+function debounce(fn, delay) {
+  let timer;
+  return function () {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, arguments);
+    }, delay);
+  };
+}
+
+function handleSearch() {
+  fetch("./data.json")
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      let value = document.getElementById("search-country").value;
+      let filteredData = data.filter((item) => {
+        return item.name.toLowerCase().includes(value.toLowerCase());
+      });
+      let html = "";
+      filteredData.forEach((item) => {
+        html += `<option value="${item.name}">${item.name}</option>`;
+      });
+
+      document.getElementById("programmingLanguages").innerHTML = html;
+    });
+}
+
+const search = debounce(handleSearch, 2000);
